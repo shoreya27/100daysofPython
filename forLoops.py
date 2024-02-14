e9bfd7c403a0f1d1a7bb97ca598590d472e2fabd
shoreya27 <54816661+shoreya27@users.noreply.github.com>
2024-02-14 08:16:58 +0530

    Pypy Juice Corner
    Concenpt - For Loop

---
+++ forLoops.py
@@ -0,0 +1,7 @@
+fruits = ['Apple', 'Bananas', 'Litchis', 'Oranges']
+
+print('Welcome to PyPy Juice Cafe ...\nServing Authenticity Since 1902 ')
+print()
+for fruit in fruits:
+    print(f'{fruit} Juice Avaliable\n')
+print('We hope you enjoy the freshness of juice\nStay Healthy!\n')
