4fa892c6f9472c5682e7d9d89363a63ec3284db7
shoreya27 <54816661+shoreya27@users.noreply.github.com>
2024-02-17 12:41:56 +0530

    Hangeman Game: Display List
    showing the display list with user input

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,15 +1,19 @@
-#Step 1 
 import random
 word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-word = random.choice(word_list)
-print(f'word: {word}')
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('Guess a letter: ').lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for character in word:
-    if character == guess:
-        print('Right')
-    else:
-        print('Wrong')
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+
+display = ['_']*len(chosen_word)
+guess = input("Guess a letter: ").lower()
+
+#TODO-2: - Loop through each position in the chosen_word;
+
+for index in range(0, len(chosen_word)):
+    if chosen_word[index] == guess:
+        display[index] = guess
+
+print(display)
