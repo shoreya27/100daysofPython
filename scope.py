3cc8ab5b3817ebf63762fd1d0b4f1ca92a69401a
shoreya27 <54816661+shoreya27@users.noreply.github.com>
2024-02-19 22:08:03 +0530

    Scope Local vs Gloabl

---
+++ scope.py
@@ -0,0 +1,16 @@
+####### Scope ########
+
+player_health = 10
+
+def game():
+    print("Inside Game Function")
+    player_health = 4
+    print(player_health)
+    
+    a = 10
+    print(a)
+
+game()
+print("Global Noww")
+print(player_health)
+# print(a)
