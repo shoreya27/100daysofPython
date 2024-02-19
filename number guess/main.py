36646d3c45ae9a7b83181f8493557f6a17a07db4
shoreya27 <54816661+shoreya27@users.noreply.github.com>
2024-02-19 22:23:26 +0530

    Number Guessing project

---
+++ number guess/main.py
@@ -0,0 +1,7 @@
+from logo import logo
+
+print(logo)
+print("I am thinking of number between 1 - 100.\nPlease guess the number")
+print('1 - Hard\n2 - Easy.\nSelect your options below by inputing 1 or 2.')
+user_guess = int(input())
+
